22bbebb401fb51609914e0ed5d019577e4fc0315
Hlewagastiz <bnjmnlrsn@gmail.com>
2020-09-11 10:15:53 -0400

    Updated white pawn movement, add movement for black pawns

--- src/com/chess/engine/pieces/Pawn.java
+++ src/com/chess/engine/pieces/Pawn.java
@@ -79,6 +79,64 @@ public class Pawn extends Piece {
                     possibleMoves.add(move);
                 }
             }
+        }else if(colour == Colour.BLACK){
+            if(coordinate <= 38){
+                newCoordinate = coordinate + 20;
+                Square newSquare = board.getSquare(newCoordinate);
+                Square checkSquare = board.getSquare(coordinate + 10);
+                if(board.isValidSquare(newCoordinate)
+                        && (!(newSquare.isOccupied())
+                        && !(checkSquare.isOccupied()))){
+                    Move move = new Move.PieceMove(coordinate, newCoordinate, this);
+                    possibleMoves.add(move);
+                }
+                newCoordinate = coordinate + 10;
+                newSquare = board.getSquare(newCoordinate);
+                if(board.isValidSquare(newCoordinate) && (!(newSquare.isOccupied()))){
+                    Move move = new Move.PieceMove(coordinate, newCoordinate, this);
+                    possibleMoves.add(move);
+                }
+                newCoordinate = coordinate + 11;
+                newSquare = board.getSquare(newCoordinate);
+                if(board.isValidSquare(newCoordinate)
+                        && (newSquare.isOccupied()
+                        && newSquare.getPiece().getColour() != colour)) {
+                    Move move = new Move.Capture(coordinate, newCoordinate, this, newSquare.getPiece());
+                    possibleMoves.add(move);
+                }
+                newCoordinate = coordinate + 9;
+                newSquare = board.getSquare(newCoordinate);
+                if(board.isValidSquare(newCoordinate)
+                        && (newSquare.isOccupied()
+                        && newSquare.getPiece().getColour() != colour)) {
+                    Move move = new Move.Capture(coordinate, newCoordinate, this, newSquare.getPiece());
+                    possibleMoves.add(move);
+                }
+            }
+            else if(coordinate > 38 && coordinate < 81) {
+                newCoordinate = coordinate + 10;
+                Square newSquare = board.getSquare(newCoordinate);
+                if(board.isValidSquare(newCoordinate) && (!(newSquare.isOccupied()))){
+                    Move move = new Move.PieceMove(coordinate, newCoordinate, this);
+                    possibleMoves.add(move);
+                }
+                newCoordinate = coordinate + 11;
+                newSquare = board.getSquare(newCoordinate);
+                if(board.isValidSquare(newCoordinate)
+                        && (newSquare.isOccupied()
+                        && newSquare.getPiece().getColour() != colour)) {
+                    Move move = new Move.Capture(coordinate, newCoordinate, this, newSquare.getPiece());
+                    possibleMoves.add(move);
+                }
+                newCoordinate = coordinate + 9;
+                newSquare = board.getSquare(newCoordinate);
+                if(board.isValidSquare(newCoordinate)
+                        && (newSquare.isOccupied()
+                        && newSquare.getPiece().getColour() != colour)) {
+                    Move move = new Move.Capture(coordinate, newCoordinate, this, newSquare.getPiece());
+                    possibleMoves.add(move);
+                }
+            }
         }
         return possibleMoves;
     }
